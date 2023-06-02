6dcccf73b1217c6aad2df6845caaaa3150a09cd7
jowynd <jowynd12@gmail.com>
2023-06-02 17:19:27 -0300

    teste

---
+++ src/br/com/alura/screenmatch/Principal/PrincipalComBusca.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch.Principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=3f75c351"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
